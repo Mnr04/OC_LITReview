18c28591a4578175d0a98008f45dc28e179efba1
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2025-12-19 19:35:42 +0100

    Add review model

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from reviews.models import User, Ticket
+from reviews.models import User, Ticket, Review
 
 
 # Here we tell Django to display data in the admin panel
 
 admin.site.register(User)
 admin.site.register(Ticket)
+admin.site.register(Review)
--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class User(AbstractUser):
     pass
@@ -13,3 +14,16 @@ class Ticket(models.Model):
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
+    headline = models.CharField(max_length=128)
+    body = models.CharField(max_length=8192, blank=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.headline
